c3e05386cf4281cb4ad853e1d517d15d57daa196
eanur rahman <lihan.lihu@gmail.com>
2024-02-24 19:31:26 +0600

    added navbar

--- src/Home/Navbar.jsx
+++ src/Home/Navbar.jsx
@@ -1,33 +1,116 @@
-import React from 'react';
+import React, { useState } from "react";
+import vector from "../assets/Vector.png";
+import { FiPhone, FiChevronDown } from "react-icons/fi";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
   return (
     <nav className="bg-gray-100 p-4">
       <div className="container mx-auto flex items-center justify-between">
-        {/* Left side - CloudFlow logo */}
         <div className="flex items-center">
-          <svg className="h-6 w-6 text-gray-900 mr-2" fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M2 4a1 1 0 0 1 1-1h14a1 1 0 0 1 0 2H3a1 1 0 0 1-1-1zm0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 0 2H3a1 1 0 0 1-1-1zm0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 0 2H3a1 1 0 0 1-1-1zm0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 0 2H3a1 1 0 0 1-1-1z" clipRule="evenodd" />
-          </svg>
-          <span className="text-gray-900">CloudFlow</span>
+          <img src={vector} alt="" />
+          <span className="ml-2 mb-1 font-bold text-2xl text-gray-900">
+            CloudFlow
+          </span>
         </div>
-        {/* Middle - Nav Links */}
-        <div className="hidden md:flex flex-grow justify-center">
-          <a href="#" className="text-gray-900 font-semibold mx-4">Home</a>
-          <a href="#" className="text-gray-900 font-semibold mx-4">About Us</a>
-          <a href="#" className="text-gray-900 font-semibold mx-4">Services</a>
-          <a href="#" className="text-gray-900 font-semibold mx-4">Projects</a>
-          <a href="#" className="text-gray-900 font-semibold mx-4">Blogs</a>
-          <a href="#" className="text-gray-900 font-semibold mx-4">Page</a>
-          <a href="#" className="text-gray-900 font-semibold mx-4">Contact</a>
+        {/* Medium Devices */}
+        <div className="md:flex items-center justify-center hidden">
+          <NavLinkWithDropdown text="Home" />
+          <NavLinkWithDropdown text="About Us" />
+          <NavLinkWithDropdown text="Services" />
+          <NavLinkWithDropdown text="Projects" />
+          <NavLinkWithDropdown text="Blogs" />
+          <NavLinkWithDropdown text="Page" />
+          <a href="#" className="text-gray-900 font-semibold mx-4">
+            Contact
+          </a>
         </div>
-        
         <div className="flex items-center">
-          <a href="#" className="text-gray-900">Need Help?</a>
+          <div className="md:hidden">
+            <button
+              onClick={() => setShowLinks(!showLinks)}
+              className="text-gray-900"
+            >
+              Menu
+            </button>
+            {showLinks && (
+              <div className="absolute top-12 right-0 w-48 bg-white border border-gray-300 z-10">
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-gray-900 hover:bg-gray-200"
+                >
+                  Home
+                </a>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-gray-900 hover:bg-gray-200"
+                >
+                  About Us
+                </a>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-gray-900 hover:bg-gray-200"
+                >
+                  Services
+                </a>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-gray-900 hover:bg-gray-200"
+                >
+                  Projects
+                </a>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-gray-900 hover:bg-gray-200"
+                >
+                  Blogs
+                </a>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-gray-900 hover:bg-gray-200"
+                >
+                  Page
+                </a>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-gray-900 hover:bg-gray-200"
+                >
+                  Contact
+                </a>
+              </div>
+            )}
+          </div>
+          <div className="hidden md:flex items-center">
+            <a
+              href="#"
+              className="text-white flex items-center justify-center rounded-full bg-gray-900 p-2"
+            >
+              <FiPhone />
+            </a>
+
+            <div className="ml-2">
+              <h2 className="text-sm font-semibold">Need help?</h2>
+              <h2 className="text-gray-900 font-bold text-xl">01716285196</h2>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
   );
-}
+};
+
+const NavLinkWithDropdown = ({ text }) => {
+    return (
+      <div className="flex items-center mx-2">
+        <a href="#" className="text-gray-900 font-semibold">
+          {text}
+        </a>
+        <FiChevronDown className="text-gray-900" />
+      </div>
+    );
+  };
+  
+  
 
 export default Navbar;
