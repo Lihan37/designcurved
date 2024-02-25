ae96fe7b3bd873929acbc3457bcf2d71fb04764e
eanur rahman <lihan.lihu@gmail.com>
2024-02-25 19:59:55 +0600

    added a heading

--- src/Home/Agency.jsx
+++ src/Home/Agency.jsx
@@ -8,12 +8,51 @@ import img9 from "../assets/img9.png";
 const Agency = () => {
   return (
     <div className="container mx-auto my-10 ">
+      <h2 className="text-4xl py-10 font-semibold text-center mb-6">
+        Where Imagination Meets 
+        <br />the Agency Innovation
+      </h2>
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <Card imgSrc={img5} title="Content and Copywriting" description="non similique voluptatem" bgColor="bg-blue-200" imgWidth={400} imgHeight={300} />
-        <Card imgSrc={img6} title="Content and Copywriting" description="consequatur excepturi magnam" bgColor="bg-green-200" imgWidth={400} imgHeight={300} />
-        <Card imgSrc={img7} title="Social Media Strategy" description="consequatur excepturi magnam" bgColor="bg-yellow-200" imgWidth={400} imgHeight={300} />
-        <Card imgSrc={img8} title="Social Media Strategy" description="consequatur excepturi magnam" bgColor="bg-pink-200" imgWidth={400} imgHeight={300} />
-        <FullWidthCard imgSrc={img9} title="Brand Identity and Logo Design" description="consequatur excepturi magnam" imgHeight={400} bgColor="bg-purple-200"  />
+        <Card
+          imgSrc={img5}
+          title="Content and Copywriting"
+          description="non similique voluptatem"
+          bgColor="#ebf8ff"
+          imgWidth={400}
+          imgHeight={300}
+        />
+        <Card
+          imgSrc={img6}
+          title="Content and Copywriting"
+          description="consequatur excepturi magnam"
+          bgColor="#f0fff4"
+          imgWidth={400}
+          imgHeight={300}
+        />
+        <Card
+          imgSrc={img7}
+          title="Social Media Strategy"
+          description="consequatur excepturi magnam"
+          bgColor="#fffbea"
+          imgWidth={400}
+          imgHeight={300}
+        />
+        <Card
+          imgSrc={img8}
+          title="Social Media Strategy"
+          description="consequatur excepturi magnam"
+          bgColor="#fff5f7"
+          imgWidth={400}
+          imgHeight={300}
+        />
+        <FullWidthCard
+          imgSrc={img9}
+          title="Brand Identity and Logo Design"
+          description="consequatur excepturi magnam"
+          imgHeight={400}
+          bgColor="#f3e8ff"
+        />
       </div>
     </div>
   );
@@ -21,27 +60,58 @@ const Agency = () => {
 
 const Card = ({ imgSrc, title, description, bgColor, imgWidth, imgHeight }) => {
   return (
-    <div className={`bg-white shadow-lg rounded-lg py-5 overflow-hidden ${bgColor} flex flex-col justify-between`}>
+    <div
+      style={{ backgroundColor: bgColor }}
+      className="bg-white shadow-lg rounded-lg py-5 overflow-hidden flex flex-col justify-between"
+    >
       <div className="flex justify-center">
-        <img src={imgSrc} alt={title} style={{ width: imgWidth, height: imgHeight }} className="object-cover mt-5" />
+        <img
+          src={imgSrc}
+          alt={title}
+          style={{ width: imgWidth, height: imgHeight }}
+          className="object-cover mt-5"
+        />
       </div>
       <div className="p-2 flex flex-col justify-center">
-        <h3 className="text-sm md:text-base lg:text-lg font-semibold mb-2 text-center">{title}</h3>
-        <p className="text-xs md:text-sm lg:text-base text-gray-600 text-center">{description}</p>
+        <h3 className="text-sm md:text-base lg:text-lg font-semibold mb-2 text-center">
+          {title}
+        </h3>
+        <p className="text-xs md:text-sm lg:text-base text-gray-600 text-center">
+          {description}
+        </p>
       </div>
     </div>
   );
 };
 
-const FullWidthCard = ({ imgSrc, title, description, bgColor, imgWidth, imgHeight }) => {
+const FullWidthCard = ({
+  imgSrc,
+  title,
+  description,
+  bgColor,
+  imgWidth,
+  imgHeight,
+}) => {
   return (
-    <div className={`bg-white shadow-lg rounded-lg overflow-hidden ${bgColor} md:col-span-2 lg:col-span-2 flex flex-col justify-between`}>
+    <div
+      style={{ backgroundColor: bgColor }}
+      className="bg-white shadow-lg rounded-lg overflow-hidden flex flex-col justify-between md:col-span-2 lg:col-span-2"
+    >
       <div className="flex justify-center">
-        <img src={imgSrc} alt={title} style={{ width: imgWidth, height: imgHeight }} className="object-cover" />
+        <img
+          src={imgSrc}
+          alt={title}
+          style={{ width: imgWidth, height: imgHeight }}
+          className="object-cover"
+        />
       </div>
       <div className="p-2 flex flex-col justify-center py-5">
-        <h3 className="text-sm md:text-base lg:text-lg font-semibold mb-2 text-center">{title}</h3>
-        <p className="text-xs md:text-sm lg:text-base text-gray-600 text-center">{description}</p>
+        <h3 className="text-sm md:text-base lg:text-lg font-semibold mb-2 text-center">
+          {title}
+        </h3>
+        <p className="text-xs md:text-sm lg:text-base text-gray-600 text-center">
+          {description}
+        </p>
       </div>
     </div>
   );
